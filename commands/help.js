604f94f3a00e43c540c6b8775181a7c0b333d14b
KM127PL <km127pl2@gmail.com>
2021-05-09 17:49:02 +0200

    Add lang support to help.js and prefix.js

--- commands/help.js
+++ commands/help.js
@@ -1,12 +1,13 @@
 const CE = require('../functions/CreateEmbed.js');
 const fs = require('fs');
+const lang = require("../lang.json")
 
 module.exports = {
-	name: 'help',
-	description: 'Wyświetla tą liste komend',
-	perms: "Brak",
+	name: lang.commands.help.name,
+	description: lang.commands.help.description,
+	perms: lang.other.perms.none,
 	execute(message, args, client) {
-		let embed = new CE("**Pomoc**", "");
+		let embed = new CE(lang.commands.help.title, "");
         const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
         for (const file of commandFiles) {
             const command = require(`./${file}`);
--- commands/prefix.js
+++ commands/prefix.js
@@ -1,29 +1,29 @@
 const CE = require('../functions/CreateEmbed.js');
 const db = require('quick.db');
-require('dotenv').config();
+const lang = require("../lang.json")
 
 module.exports = {
-	name: 'prefix',
-	description: 'Wyświetla oraz zmienia prefix bota.',
-    perms: 'Zarządzanie Wiadomościami',
+	name: lang.commands.prefix.name,
+	description: lang.commands.prefix.description,
+    perms: lang.other.perms['manage-messages'],
 	execute(message, args, client) {
         if(args.length > 0) {
             if(!message.member.hasPermission("MANAGE_MESSAGES")) {
-                let embed = new CE("Moderator", "Nie masz permisji do używania tej komendy!");
+                let embed = new CE(lang.other.moderator, lang.other['no-perm']);
 		        message.channel.send({ embed: embed.getEmbed() });
 		        message.react(process.env.BAD_EMOJI);
                 return embed.destroy();
             }
             db.set(`prefix.${message.guild.id}`, args[0]);
 
-            let embed = new CE("Moderator", "Ustawiono prefix na: " + args[0]);
+            let embed = new CE(lang.other.moderator, lang.commands.prefix['set-to'] + args[0]);
 		    message.channel.send({ embed: embed.getEmbed() });
 		    message.react(process.env.GOOD_EMOJI);
             return embed.destroy();
         }
 
 
-        let embed = new CE("Moderator", "Mój prefix tutaj to: " + db.get(`prefix.${message.guild.id}`));
+        let embed = new CE(lang.other.moderator, lang.commands.prefix['my-prefix-here'] + db.get(`prefix.${message.guild.id}`));
 		message.channel.send({ embed: embed.getEmbed() });
 		message.react(process.env.GOOD_EMOJI);
         return embed.destroy();
