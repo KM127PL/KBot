43ff5d3ac5a158893ecca2c7b2d41a7ceb0cede1
KM127PL <km127pl2@gmail.com>
2021-05-09 18:32:22 +0200

    Add a math.js command

---
+++ commands/math.js
@@ -0,0 +1,24 @@
+const CE = require('../functions/CreateEmbed.js');
+const lang = require(`../lang-${process.env.LANGUAGE}.json`);
+const math = require(`mathjs`);
+
+
+module.exports = {
+	name: lang.commands.math.name,
+	description: lang.commands.math.description,
+	perms: lang.other.perms.none,
+	execute(message, args, client) {
+		let expr = "";
+		for (let i = 0; i < args.length; i++) {
+			expr = expr + " " + args[i];
+		}
+		let embed = new CE(`${lang.commands.math.title} (${expr} )`, `\`\`\`${math.evaluate(expr)}\`\`\``);
+		message.channel.send({ embed: embed.getEmbed() });
+		message.react(process.env.GOOD_EMOJI);
+		return embed.destroy();
+		/*let embed = new CE(`${lang.commands.math.title}`, ``);
+		message.channel.send({ embed: embed.getEmbed() });
+		message.react(process.env.GOOD_EMOJI);
+		embed.destroy();*/
+	},
+};
