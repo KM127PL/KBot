f2b579db58e8ef167abad1cbb3bd74f7fbea4f40
KM127PL <km127pl2@gmail.com>
2021-05-09 17:51:43 +0200

    Add lang support to ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,14 @@
 const CE = require('../functions/CreateEmbed.js');
+const lang = require('../lang.json');
 
 module.exports = {
-	name: 'ping',
-	description: 'Wyświetla ping bota',
-	perms: "Brak",
+	name: lang.commands.ping.name,
+	description: lang.commands.ping.description,
+	perms: lang.other.perms.none,
 	execute(message, args, client) {
-		let embed = new CE("Ping 🏓", `Mój ping to ${Math.floor(Date.now() - message.createdTimestamp)}ms`);
+		let embed = new CE("Ping 🏓", `${lang.commands.ping['my-ping']} ${Math.floor(Date.now() - message.createdTimestamp)}ms`);
 		message.channel.send({ embed: embed.getEmbed() });
-		message.react('840704663468376106');
+		message.react(process.env.GOOD_EMOJI);
 		embed.destroy();
 	},
 };
