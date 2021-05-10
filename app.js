dda56cde6d55320efda3462016680f2e324239d1
KM127PL <km127pl2@gmail.com>
2021-05-10 12:16:13 +0200

    Just rename the file to .disabled ;(

--- app.js
+++ app.js
@@ -4,14 +4,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const db = require('quick.db');
 const CE = require('./functions/CreateEmbed.js');
-const { ifError } = require('assert');
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 const lang = require(`./lang/lang-${process.env.LANGUAGE}.json`);
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
-client.disabled_commands = new Discord.Collection();
-
 client.on('message', (message) => {
 	if(message.author.bot || message.channel.type === 'dm') return; // Cancel if the author is a bot or channel is dm
 	if(!db.has(`prefix.${message.guild.id}`)) return db.set(`prefix.${message.guild.id}`, `${process.env.DISCORD_PREFIX}`); // Set the guild prefix if none is present
@@ -35,10 +32,8 @@ client.on('message', (message) => {
 
 	client.events.get('on-message-sent').execute(message, message.content, client);
 	
-	if(client.disabled_commands.get(commandName.toLowerCase())) return;
 	if (!message.content.startsWith(prefix.toLowerCase())) return;
 	try {
-		if(client.disabled_commands.get(command.name.toLowerCase())) return;
 		command.execute(message, args, client);
 	} catch (error) {
 		console.error(error);
@@ -52,23 +47,18 @@ client.on('message', (message) => {
 */
 client.on('ready', () => {
 	for (const file of commandFiles) {
-		if(file.startsWith('-')) {
-			const command = require(`./commands/${file}`);
-			client.disabled_commands.set(command.name.toLowerCase(), true);
-			continue;
-		};
 		const command = require(`./commands/${file}`);
 
 		client.commands.set(command.name.toLowerCase(), command);
 		console.log(`[BOT] Registering command ${command.name.toLowerCase()} from ${file}`)
 	}
 	for (const file of eventFiles) {
-		if(file.startsWith('-')) continue;
 		const event = require(`./events/${file}`);
 		client.events.set(event.event.toLowerCase(), event);
 		console.log(`[BOT] Registering event ${event.event.toLowerCase()} from ${file}`)
 	}
     console.log(`[BOT] Logged in as ${client.user.tag}!`);
+	client.user.setActivity(`${client.guilds.cache.size} Serwery`, { type: "COMPETING"})
 });
 
 client.login(process.env.DISCORD_TOKEN);
