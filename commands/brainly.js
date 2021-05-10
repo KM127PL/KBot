631162ca431897d170fa89bbc4c26de326bc2331
KM127PL <km127pl2@gmail.com>
2021-05-10 09:19:27 +0200

    Add a experimental brainly.js command

--- commands/brainly.js
+++ commands/brainly.js
@@ -9,7 +9,12 @@ module.exports = {
 	description: lang.commands.brainly.description,
 	perms: lang.other.perms.none,
 	execute(message, args, client) {
-        if(args.length == 0) return;
+        if(args.length == 0) {
+            let error = new CE(lang.commands.brainly.title, `${lang.other['no-args']}`);
+            message.channel.send({ embed: error.getEmbed() })
+            message.react(process.env.BAD_EMOJI);
+            return error.destroy();
+        }
         let loading = new CE(lang.commands.brainly.loading, '');
         message.channel.send({ embed: loading.getEmbed() }).then(msg => {
             
@@ -20,11 +25,13 @@ module.exports = {
             }
             
             try {
-                brainly(expr, 1, process.env.LANGUAGE.toLowerCase()).then(resp => {
+                brainly(expr, 2, process.env.LANGUAGE.toLowerCase()).then(resp => {
                     let embed = new CE(lang.commands.brainly.title, '');
                     for(let i = 0; i < resp.data.length; i++) {
-                            embed.addField(parseLatex(resp.data[i].pertanyaan), parseLatex(resp.data[i].jawaban[0].text));
-                    }
+                            embed.addField(`${i + 1}. ${parseLatex(resp.data[i].pertanyaan)}`, `\`\`\`${parseLatex(resp.data[i].jawaban[0].text)}\`\`\``);
+                            embed.setFooter(`${resp.data[i].jawaban[0].media}`);
+                        }
+                    
                     msg.edit({ embed: embed.getEmbed() });
 		            message.react(process.env.GOOD_EMOJI);
 		            embed.destroy();
